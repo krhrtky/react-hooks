9b0d028766ff641cf27552a0a652376dd1028396
t-kurihara <takuya.kurihara00@gmail.com>
2019-12-15 23:27:35 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import {useCallback, useMemo, useState} from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const handleClick = useCallback(() => {
+        setCount(count + 1)
+    }, [count])
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={handleClick}>+1</button>
+        </div>
+    )
+}
+
+const Double = () => {
+    const [count, setCount] = useState(0)
+    const double = useMemo(() => count * 2, [count])
+    const doubleWithUnit = useMemo(() => `${double} pt`, [double])
+    const handleClick = useCallback(() => {
+        setCount(prevState => prevState + 1)
+    }, [])
+
+    return (
+        <div>
+            <p>cont  : {count}</p>
+            <p>double: {double}</p>
+            <p>doubleWithUnit : {doubleWithUnit}</p>
+            <button onClick={handleClick}>+1</button>
+        </div>
+    )
+}
+
+type Props = {
+    clickedX: number
+    clickedY: number
+    handleClick: (Event: React.MouseEvent<HTMLElement, MouseEvent>) => void
+}
+
+const Coordinate: React.FC<Props> = props => (
+    <div>
+        <div style={{width: 100, height: 100, background: '#ccf'}}/>
+        <p style={{ width: 100, height: 100, background: '#fcc' }} onClick={props.handleClick}/>
+        <p>X: { props.clickedX }</p>
+        <p>Y: { props.clickedY }</p>
+    </div>
+)
+
+const Wrapper = () => {
+    const [state, update] = useState({
+        clickedX: 0,
+        clickedY: 0
+    })
+    const handleClick = useCallback(
+        (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+            event.persist()
+            const { top, left } = event.currentTarget.getBoundingClientRect()
+            update(prevState => ({
+                ...prevState,
+                clickedX: event.clientX - left,
+                clickedY: event.clientY - top
+            }))
+        },
+        []
+    )
+    return (
+        <Coordinate
+            clickedX={state.clickedX}
+            clickedY={state.clickedY}
+            handleClick={handleClick}
+        />
+    )
+}
+
+const Title = () => <h1>hooks sample</h1>
+
+const App = () => (
+    <div>
+        <Title/>
+        <Counter/>
+        <Double/>
+        <Wrapper/>
+    </div>
+)
+
+render(<App/>, document.getElementById('app'))
