ac83cfb934b380658dbd50ba95bc223049d01ed0
t-kurihara <takuya.kurihara00@gmail.com>
2019-12-22 15:22:13 +0900

    [tmp] legacy action type definitions.

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,25 +1,9 @@
+import { Actions } from './legacyActions'
+
 type State = {
     count: number
     unit: string
 }
-
-type IncrementAction = {
-    type: 'INCREMENT'
-}
-
-type DecrementAction = {
-    type: 'DECREMENT'
-}
-
-type SetCountAction = {
-    type: 'SET_COUNT'
-    payload: {
-        amount: number
-    }
-}
-
-type Actions = IncrementAction | DecrementAction | SetCountAction
-
 export const initialState: State = {
     count: 0,
     unit: 'pt'
@@ -28,12 +12,12 @@ export const initialState: State = {
 export const reducer: (state: State, action: Actions) => State =
     (state , action) => {
         switch (action.type) {
-            case 'INCREMENT':
-                return { ...state, count: state.count + 1 }
-            case 'DECREMENT':
-                return { ...state, count: state.count - 1 }
-            case 'SET_COUNT':
-                return { ...state, count: action.payload.amount }
+            // case :
+            //     return { ...state, count: state.count + 1 }
+            // case 'DECREMENT':
+            //     return { ...state, count: state.count - 1 }
+            // case 'SET_COUNT':
+            //     return { ...state, count: action.payload.amount }
             default:
                 throw new Error()
         }
